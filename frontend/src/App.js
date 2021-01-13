044b8992671d09e0f1b64e7cef3cc368700f1e96
hmbt93@gmail.com <hmbt93@gmail.com>
2021-01-13 15:27:38 +0700

    update index

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,47 @@
-import './App.scss';
+import "antd/dist/antd.css";
+import "./assets/css/global.scss";
+import React, { useState } from "react";
+import HeaderCustomize from "./components/Header/Header";
 
+// import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Layout, Menu } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+const { Content, Sider } = Layout;
+const { SubMenu } = Menu;
 function App() {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const handleCollapsed = () => setCollapsed(!collapsed);
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1> Hello My Team   </h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout>
+<HeaderCustomize/>
+      <Content style={{ padding: "150px 200px 0px 200px" }}>
+        <Layout
+          className="site-layout-background"
+          style={{ padding: "24px 0" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Sider className="site-layout-background" width={200}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={["1"]}
+              defaultOpenKeys={["sub1"]}
+              style={{ height: "100%" }}
+            >
+              <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+                <Menu.Item key="1">option1</Menu.Item>
+                <Menu.Item key="2">option2</Menu.Item>
+                <Menu.Item key="3">option3</Menu.Item>
+                <Menu.Item key="4">option4</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Content style={{ padding: "0 24px", minHeight: 280 }}>
+            Content
+          </Content>
+        </Layout>
+      </Content>
+    </Layout>
   );
 }
 
---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,80 @@
+import {Layout, Row, Col, Button,Input } from "antd";
+
+import {
+  ShoppingCartOutlined,
+  HeartOutlined,
+  BellOutlined,
+} from "@ant-design/icons";
+const { Header } = Layout;
+const { Search } = Input;
+
+const onSearch = (value) => console.log(value);
+
+const HeaderCustomize = () => {
+  return (
+    <Header className="header">
+      <div className="header_top">
+        <ul class="menu_top">
+          <li>
+            <a href="/">Kích hoạt khóa học</a>
+          </li>
+          <li>
+            <a href="/">Kiểm tra đơn hàng</a>
+          </li>
+          <li>
+            <a href="/">Chăm sóc khách hàng</a>
+          </li>
+          <li>
+            <a href="/">Trở thành giảng viên</a>
+          </li>
+          <li>
+            <a href="/">Khóa học của tôi</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="header_main">
+        <Row>
+          <Col span={6}>
+            <div className="header_main_logo">
+              <a href="/">
+                <img
+                  src={process.env.PUBLIC_URL + "/logo.png"}
+                  className="logo"
+                  alt=""
+                />
+              </a>
+            </div>
+          </Col>
+
+          <Col span={9}>
+            <div className="header_main_search">
+              <Search placeholder="tìm kiếm...." onSearch={onSearch} />
+            </div>
+          </Col>
+          <Col span={4}>
+            <div className="header_main_button">
+              <Button
+                type="primary"
+                icon={<ShoppingCartOutlined />}
+                size={"large"}
+              />
+              <Button type="primary" icon={<HeartOutlined />} size={"large"} />
+              <Button type="primary" icon={<BellOutlined />} size={"large"} />
+            </div>
+          </Col>
+          <Col span={5}>
+            <div className="header_main_user">
+              <Button className="btn_academy btn_dangnhap">Đăng Nhập</Button>
+              <Button danger className="btn_academy btn_dangky">
+                Đăng Ký
+              </Button>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </Header>
+  );
+};
+
+export default HeaderCustomize;
