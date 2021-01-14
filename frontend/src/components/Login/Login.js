46dbf5c30afc1bd373cc908c57277456c8bbae72
hmbt93@gmail.com <hmbt93@gmail.com>
2021-01-14 16:54:32 +0700

    commen font rubik

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -30,7 +30,8 @@ const Login = () => {
                       Login to your <span>Account</span>
                     </h2>
                     <p>
-                      Don't have an account?
+                    Lorem Ipsum là gì?
+Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những đã tồn tại năm thế kỉ, mà khi được áp dụng vào tin học văn phòng, nội dung của nó vẫn không hề bị thay đổi. Nó đã được phổ biến trong những năm 1960 nhờ việc bán những bản giấy Letraset in những đoạn Lorem Ipsum, và gần đây hơn, được sử dụng trong các ứng dụng dàn trang, như Aldus PageMaker.
                       <Link to="/register"> Create one here </Link>
                     </p>
                   </div>
