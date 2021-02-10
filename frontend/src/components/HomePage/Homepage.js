476f4e029ded034e503cf3165f46adf084ec1c5c
Nguyễn Thị Oanh <46190679+oanh1660422@users.noreply.github.com>
2021-02-10 20:03:38 +0700

    Crearte page

--- frontend/src/components/HomePage/Homepage.js
+++ frontend/src/components/HomePage/Homepage.js
@@ -1,10 +1,15 @@
 import React from "react";
 import Slide from "./Slide/Slide"
+import Page from "./Page/Page"
 
 const Homepage = () => {
   return (
     <div>
       <Slide/>
+      <Page title='yeuthichnhat' />
+      <Page title='xemnhieunhat' />
+      <Page title='muanhieunhat' />
+      <Page title='moinhat' />
     </div>
   );
 };
