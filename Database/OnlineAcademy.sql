78081ab0e423e7bfe53e60b20d0bba4e3f0126fc
Nguyễn Thị Oanh <46190679+oanh1660422@users.noreply.github.com>
2021-02-23 18:37:02 +0700

    Update database

--- Database/OnlineAcademy.sql
+++ Database/OnlineAcademy.sql
@@ -1,186 +1,136 @@
-/*
- Navicat Premium Data Transfer
 
- Source Server         : mysqlwbb
- Source Server Type    : MySQL
- Source Server Version : 80022
- Source Host           : localhost:3366
- Source Schema         : onlineacademy
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+07:00";
 
- Target Server Type    : MySQL
- Target Server Version : 80022
- File Encoding         : 65001
+DROP TABLE IF EXISTS `CategoryGroup`;
+CREATE TABLE IF NOT EXISTS `CategoryGroup` (
+  `CategoryGroupId` int(11) NOT NULL AUTO_INCREMENT,
+  `CategoryGroupName` varchar(255) DEFAULT NULL,
+  `Created_At` timestamp NULL DEFAULT NULL,
+  `Image` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `IsActive` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`CategoryGroupId`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
- Date: 05/02/2021 20:45:03
-*/
+DROP TABLE IF EXISTS `Category`;
+CREATE TABLE IF NOT EXISTS `Category` (
+  `CategoryId` int(11) NOT NULL AUTO_INCREMENT,
+  `CategoryName` varchar(255) DEFAULT NULL,
+  `Note` varchar(10000) DEFAULT NULL,
+  `Rate` float(11) DEFAULT '0',
+  `QuanRes` int(11) DEFAULT '0',
+  `TeacherID` int(11) DEFAULT NULL,
+  `Image` longtext,
+  `Price` decimal(18,2) DEFAULT NULL,
+  `CategoryGroupId` int(11) DEFAULT NULL,
+  `Created_At` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `Update_At` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `IsActive` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`CategoryId`) USING BTREE,
+  KEY `CategoryGroupId` (`CategoryGroupId`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for buypoint
--- ----------------------------
-DROP TABLE IF EXISTS `buypoint`;
-CREATE TABLE `buypoint`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
-  `Usersid` int(0) NULL DEFAULT NULL,
-  `Point` decimal(18, 2) NULL DEFAULT NULL,
-  `Created_at` timestamp(0) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `Usersid`(`Usersid`) USING BTREE,
-  CONSTRAINT `buypoint_ibfk_1` FOREIGN KEY (`Usersid`) REFERENCES `users` (`Usersid`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for category
--- ----------------------------
-DROP TABLE IF EXISTS `category`;
-CREATE TABLE `category`  (
-  `Categoryid` int(0) NOT NULL AUTO_INCREMENT,
-  `Categoryname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `Note` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `Notedetail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `Quanlike` int(0) NULL DEFAULT NULL,
-  `Quanres` int(0) NULL DEFAULT NULL,
-  `Picture` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `Price` decimal(18, 2) NULL DEFAULT NULL,
-  `CategoryGroupid` int(0) NULL DEFAULT NULL,
-  `Created_at` timestamp(0) NULL DEFAULT NULL,
-  `Update_at` timestamp(0) NULL DEFAULT NULL,
-  `Isactive` bit(1) NULL DEFAULT NULL,
-  PRIMARY KEY (`Categoryid`) USING BTREE,
-  INDEX `CategoryGroupid`(`CategoryGroupid`) USING BTREE,
-  CONSTRAINT `category_ibfk_1` FOREIGN KEY (`CategoryGroupid`) REFERENCES `categorygroup` (`CategoryGroupid`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for categorygroup
--- ----------------------------
-DROP TABLE IF EXISTS `categorygroup`;
-CREATE TABLE `categorygroup`  (
-  `CategoryGroupid` int(0) NOT NULL AUTO_INCREMENT,
-  `CategorGroupname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `Created_at` timestamp(0) NULL DEFAULT NULL,
-  `Isactive` bit(1) NULL DEFAULT NULL,
-  PRIMARY KEY (`CategoryGroupid`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for discount
--- ----------------------------
-DROP TABLE IF EXISTS `discount`;
-CREATE TABLE `discount`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
-  `Categoryid` int(0) NULL DEFAULT NULL,
-  `Value` int(0) NULL DEFAULT NULL,
-  `Fromdate` timestamp(0) NULL DEFAULT NULL,
-  `Enddate` timestamp(0) NULL DEFAULT NULL,
-  `Created_at` timestamp(0) NULL DEFAULT NULL,
-  `Isactive` bit(1) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `Categoryid`(`Categoryid`) USING BTREE,
-  CONSTRAINT `discount_ibfk_1` FOREIGN KEY (`Categoryid`) REFERENCES `category` (`Categoryid`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for job
--- ----------------------------
-DROP TABLE IF EXISTS `job`;
-CREATE TABLE `job`  (
-  `Jobid` int(0) NOT NULL AUTO_INCREMENT,
-  `Jobname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `Isactive` bit(1) NULL DEFAULT NULL,
-  PRIMARY KEY (`Jobid`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of job
--- ----------------------------
-INSERT INTO `job` VALUES (1, 'Admin', b'1');
-INSERT INTO `job` VALUES (2, 'Học Viên', b'1');
-INSERT INTO `job` VALUES (3, 'Giảng Viên', b'1');
-
--- ----------------------------
--- Table structure for likedetail
--- ----------------------------
-DROP TABLE IF EXISTS `likedetail`;
-CREATE TABLE `likedetail`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
-  `Usersid` int(0) NULL DEFAULT NULL,
-  `Categoryid` int(0) NULL DEFAULT NULL,
-  `Liketime` timestamp(0) NULL DEFAULT NULL,
-  `Isactive` bit(1) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `Categoryid`(`Categoryid`) USING BTREE,
-  INDEX `Usersid`(`Usersid`) USING BTREE,
-  CONSTRAINT `likedetail_ibfk_1` FOREIGN KEY (`Categoryid`) REFERENCES `category` (`Categoryid`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `likedetail_ibfk_2` FOREIGN KEY (`Usersid`) REFERENCES `users` (`Usersid`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for product
--- ----------------------------
-DROP TABLE IF EXISTS `product`;
-CREATE TABLE `product`  (
-  `Productid` int(0) NOT NULL AUTO_INCREMENT,
-  `NumberNo` int(0) NULL DEFAULT NULL,
-  `Productname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `Video` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+DROP TABLE IF EXISTS `Product`;
+CREATE TABLE IF NOT EXISTS `Product` (
+  `ProductId` int(11) NOT NULL AUTO_INCREMENT,
+  `NumberNo` int(11) DEFAULT NULL,
+  `ProductName` varchar(255) DEFAULT NULL,
+  `Video` varchar(255) DEFAULT NULL,
+  `View` int DEFAULT 0,
   `Public` bit(1) NULL DEFAULT NULL,
-  `Categoryid` int(0) NULL DEFAULT NULL,
-  `Created_at` timestamp(0) NULL DEFAULT NULL,
-  `Isactive` bit(1) NULL DEFAULT NULL,
-  PRIMARY KEY (`Productid`) USING BTREE,
-  INDEX `Categoryid`(`Categoryid`) USING BTREE,
-  CONSTRAINT `product_ibfk_1` FOREIGN KEY (`Categoryid`) REFERENCES `category` (`Categoryid`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  `CategoryId` int(11) DEFAULT NULL,
+  `Created_At` timestamp NULL DEFAULT NULL,
+  `IsActive` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`ProductId`) USING BTREE,
+  KEY `CategoryId` (`CategoryId`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Table structure for resdetail
--- ----------------------------
-DROP TABLE IF EXISTS `resdetail`;
-CREATE TABLE `resdetail`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
-  `Usersid` int(0) NULL DEFAULT NULL,
-  `Categoryid` int(0) NULL DEFAULT NULL,
-  `Costeach` decimal(18, 2) NULL DEFAULT NULL,
-  `Note` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `Restime` timestamp(0) NULL DEFAULT NULL,
-  `Isactive` bit(1) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `Categoryid`(`Categoryid`) USING BTREE,
-  INDEX `Usersid`(`Usersid`) USING BTREE,
-  CONSTRAINT `resdetail_ibfk_1` FOREIGN KEY (`Categoryid`) REFERENCES `category` (`Categoryid`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-  CONSTRAINT `resdetail_ibfk_2` FOREIGN KEY (`Usersid`) REFERENCES `users` (`Usersid`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+DROP TABLE IF EXISTS `Job`;
+CREATE TABLE IF NOT EXISTS `Job` (
+  `JobId` int(11) NOT NULL AUTO_INCREMENT,
+  `JobName` varchar(255) DEFAULT NULL,
+  `IsActive` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`Jobid`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Table structure for users
--- ----------------------------
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users`  (
-  `Usersid` int(0) NOT NULL AUTO_INCREMENT,
-  `Email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `Password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `Dislayname` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `Point` decimal(18, 2) NULL DEFAULT NULL,
-  `Jobid` int(0) NULL DEFAULT NULL,
-  `Created_at` timestamp(0) NULL DEFAULT NULL,
-  `Isactive` bit(1) NULL DEFAULT NULL,
-  `rfToken` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `Telephone` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `OTP` int(0) NULL DEFAULT NULL,
-  `OTP_Confim` bit(1) NULL DEFAULT b'0',
-  PRIMARY KEY (`Usersid`) USING BTREE,
-  INDEX `Jobid`(`Jobid`) USING BTREE,
-  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`Jobid`) REFERENCES `job` (`Jobid`) ON DELETE RESTRICT ON UPDATE RESTRICT
-) ENGINE = InnoDB AUTO_INCREMENT = 1036 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+DROP TABLE IF EXISTS `Users`;
+CREATE TABLE IF NOT EXISTS `Users` (
+  `UsersId` int(11) NOT NULL AUTO_INCREMENT,
+  `Email` varchar(255) DEFAULT NULL,
+  `Password` varchar(255) DEFAULT NULL,
+  `DislayName` varchar(255) DEFAULT NULL,
+  `Image` longtext CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
+  `Point` decimal(18,2) DEFAULT NULL,
+  `JobId` int(11) DEFAULT NULL,
+  `Created_At` timestamp NULL DEFAULT NULL,
+  `rfToken` varchar(255) DEFAULT NULL,
+  `Telephone` varchar(255) DEFAULT NULL,
+  `OTP` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `OTP_Confim` bit(1) DEFAULT b'0',
+  `IsActive` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`UsersId`) USING BTREE,
+  KEY `Jobid` (`JobId`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
--- ----------------------------
--- Records of users
--- ----------------------------
-INSERT INTO `users` VALUES (1000, 'hmbt', '$2a$04$iKtF44brw0HVA19d9KsPh.lg3Ma4uv1jiV20Sr.r7VUmGMucaG8pa', 'hà toàn', 123.00, 2, '2021-01-13 19:39:23', b'1', 'aDrUzrcrIMUY4oZpXk0pQwmFNO12OktF', NULL, NULL, b'1');
-INSERT INTO `users` VALUES (1008, 'admin', '$2a$04$iKtF44brw0HVA19d9KsPh.lg3Ma4uv1jiV20Sr.r7VUmGMucaG8pa', 'Admin', NULL, 1, NULL, NULL, '0qKr4OrSkc7bGn2XXBSMo3v0XeIPQzuq', NULL, NULL, b'1');
-INSERT INTO `users` VALUES (1035, 'hmbt93@gmail.com', '$2a$04$g4wfvE1T11mSqIBfXLXc6.RpdWqPjU9cWTtcG.6Ls.FvMq7zgCtBO', 'Hà Minh Bảo Toàn', NULL, 2, '2021-02-04 09:40:54', NULL, 'PahNK2qSmf1QmiG3IrZBQzHY7cW66rh5', NULL, 611051, b'1');
+DROP TABLE IF EXISTS `BuyPoint`;
+CREATE TABLE IF NOT EXISTS `BuyPoint` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `UsersId` int(11) DEFAULT NULL,
+  `Point` decimal(18,2) DEFAULT NULL,
+  `Created_At` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`Id`) USING BTREE,
+  KEY `UsersId` (`UsersId`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
-SET FOREIGN_KEY_CHECKS = 1;
+DROP TABLE IF EXISTS `Discount`;
+CREATE TABLE IF NOT EXISTS `Discount` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `CategoryId` int(11) DEFAULT NULL,
+  `Value` int(11) DEFAULT NULL,
+  `Fromdate` timestamp NULL DEFAULT NULL,
+  `Enddate` timestamp NULL DEFAULT NULL,
+  `Created_At` timestamp NULL DEFAULT NULL,
+  `IsActive` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`Id`) USING BTREE,
+  KEY `CategoryId` (`CategoryId`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+DROP TABLE IF EXISTS `RateDetail`;
+CREATE TABLE IF NOT EXISTS `RateDetail` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `UsersId` int(11) DEFAULT NULL,
+  `CategoryId` int(11) DEFAULT NULL,
+  `RateValue` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`) USING BTREE,
+  KEY `CategoryId` (`CategoryId`) USING BTREE,
+  KEY `UsersId` (`UsersId`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+DROP TABLE IF EXISTS `ResDetail`;
+CREATE TABLE IF NOT EXISTS `ResDetail` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `UsersId` int(11) DEFAULT NULL,
+  `CategoryId` int(11) DEFAULT NULL,
+  `CostEach` decimal(18,2) DEFAULT NULL,
+  `ResTime` timestamp NULL DEFAULT NULL,
+  `IsActive` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`Id`) USING BTREE,
+  KEY `CategoryId` (`UsersId`) USING BTREE,
+  KEY `UsersId` (`Usersid`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+DROP TABLE IF EXISTS `CmtDetail`;
+CREATE TABLE IF NOT EXISTS `CmtDetail` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `UsersId` int(11) DEFAULT NULL,
+  `CategoryId` int(11) DEFAULT NULL,
+  `Cmt` varchar(10000) DEFAULT NULL,
+  `CmtTime` timestamp NULL DEFAULT NULL,
+  `IsActive` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`Id`) USING BTREE,
+  KEY `CategoryId` (`UsersId`) USING BTREE,
+  KEY `UsersId` (`Usersid`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+
