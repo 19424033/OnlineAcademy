3e72f6263965565766a818dfc6a292bb0e2007fe
Nguyễn Thị Oanh <46190679+oanh1660422@users.noreply.github.com>
2021-02-28 16:57:29 +0700

    update coursesdetail

--- frontend/src/containers/CoursesDetail/BuyCourses/BuyCourses.js
+++ frontend/src/containers/CoursesDetail/BuyCourses/BuyCourses.js
@@ -4,54 +4,47 @@ import {
 } from "react-router-dom";
 import axios from "axios";
 
-const BuyCourses = () => {
+const BuyCourses = (props) => {
+
     return(
-    <div className="col-lg-3 col-md-4 col-sm-12 m-b30">
-    <div className="course-detail-bx">
-      <div className="course-price">
-        <del>$190</del>
-        <h4 className="price">$120</h4>
-      </div>	
-      <div className="course-buy-now text-center">
-        <a href="#" className="btn radius-xl text-uppercase">Buy Now This Courses</a>
-      </div>
-      <div className="teacher-bx">
-        <div className="teacher-info">
-          <div className="teacher-thumb">
-            <img src="assets/images/testimonials/pic1.jpg" alt />
+    <div className="col-lg-3 col-md-4 col-sm-12 m-b30" >
+      <div className="course-detail-bx" >
+        <div className="course-price">
+          { props.categories.value != null
+            ? <>
+                <del> { parseInt(props.categories.Price).toLocaleString() } đ </del>
+                <h5>{ (props.categories.Price * (100-props.categories.value) /100).toLocaleString()  } đ </h5> 
+              </>
+            : <> 
+                <br></br><h5>{ parseInt(props.categories.Price).toLocaleString() } đ </h5> 
+              </> 
+          }
+        </div>	
+        <div className="course-buy-now text-center">
+          <a className="btn radius-xl text-uppercase">Đăng Ký Khóa Học</a>
+        </div>
+        <div className="teacher-bx">
+          <div className="teacher-info">
+            <div className="teacher-thumb">
+              <img src={`data:image/jpg;base64,${props.categories.Ava}`} />
+            </div>
+            <div className="teacher-name">
+              <h5>{ props.categories.DislayName }</h5>
+            </div>
           </div>
-          <div className="teacher-name">
-            <h5>Hinata Hyuga</h5>
-            <span>Science Teacher</span>
+        </div>
+        <div className="cours-more-info">
+          <div style={{width:'50%' , textAlign:'center'}}>
+            <h4> { props.categories.Rate }/5 <i className="fa fa-star" style={{color:'#FFFF33'}} /></h4>  
+            <span>{ props.categories.TotalRate } Đánh giá </span>
           </div>
-        </div>
+          <div style={{width:'50%' , textAlign:'center'}}>
+            <h4> { props.categories.QuanRes } <i className="fa fa-user" /></h4>  
+            <span>Đã đăng ký</span>
+          </div>
       </div>
-      <div className="cours-more-info">
-        <div className="review">
-          <span>3 Review</span>
-          <ul className="cours-star">
-            <li className="active"><i className="fa fa-star" /></li>
-            <li className="active"><i className="fa fa-star" /></li>
-            <li className="active"><i className="fa fa-star" /></li>
-            <li><i className="fa fa-star" /></li>
-            <li><i className="fa fa-star" /></li>
-          </ul>
-        </div>
-        <div className="price categories">
-          <span>Categories</span>
-          <h5 className="text-primary">Frontend</h5>
-        </div>
-      </div>
-      <div className="course-info-list scroll-page">
-        <ul className="navbar">
-          <li><a className="nav-link" href="#overview"><i className="ti-zip" />Overview</a></li>
-          <li><a className="nav-link" href="#curriculum"><i className="ti-bookmark-alt" />Curriculum</a></li>
-          <li><a className="nav-link" href="#instructor"><i className="ti-user" />Instructor</a></li>
-          <li><a className="nav-link" href="#reviews"><i className="ti-comments" />Reviews</a></li>
-        </ul>
       </div>
     </div>
-  </div>
     )
 }
 
